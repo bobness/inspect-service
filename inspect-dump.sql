c5e764a352d2377a8f86089ecbfae2e3f7a0ee47
Steve Herzog <herzogdev@gmail.com>
2022-06-08 13:08:06 -0700

    fixed query issue by version

--- inspect-dump.sql
+++ inspect-dump.sql
@@ -6,15 +6,12 @@
 -- Dumped by pg_dump version 13.7
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
-SET row_security = off;
 
 --
 -- Name: adminpack; Type: EXTENSION; Schema: -; Owner: -
@@ -23,16 +20,7 @@ SET row_security = off;
 CREATE EXTENSION IF NOT EXISTS adminpack WITH SCHEMA pg_catalog;
 
 
---
--- Name: EXTENSION adminpack; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION adminpack IS 'administrative functions for PostgreSQL';
-
-
 SET default_tablespace = '';
-
-SET default_table_access_method = heap;
 
 --
 -- Name: comments; Type: TABLE; Schema: public; Owner: postgres
@@ -51,20 +39,6 @@ CREATE TABLE public.comments (
 ALTER TABLE public.comments OWNER TO postgres;
 
 --
--- Name: comments_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-ALTER TABLE public.comments ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.comments_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
-
-
---
 -- Name: followers; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -77,19 +51,6 @@ CREATE TABLE public.followers (
 
 
 ALTER TABLE public.followers OWNER TO postgres;
-
---
--- Name: followers_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-ALTER TABLE public.followers ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.followers_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
 
 
 --
@@ -109,20 +70,6 @@ CREATE TABLE public.reactions (
 ALTER TABLE public.reactions OWNER TO postgres;
 
 --
--- Name: reactions_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-ALTER TABLE public.reactions ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.reactions_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
-
-
---
 -- Name: snippets; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -134,20 +81,6 @@ CREATE TABLE public.snippets (
 
 
 ALTER TABLE public.snippets OWNER TO postgres;
-
---
--- Name: snippets_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-ALTER TABLE public.snippets ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.snippets_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
-
 
 --
 -- Name: summaries; Type: TABLE; Schema: public; Owner: postgres
@@ -165,19 +98,6 @@ CREATE TABLE public.summaries (
 
 ALTER TABLE public.summaries OWNER TO postgres;
 
---
--- Name: summaries_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-ALTER TABLE public.summaries ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.summaries_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
-
 
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
@@ -193,19 +113,6 @@ CREATE TABLE public.users (
 
 
 ALTER TABLE public.users OWNER TO postgres;
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-ALTER TABLE public.users ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.users_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
 
 
 --
